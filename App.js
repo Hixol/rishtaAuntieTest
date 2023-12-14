c875178e55e4e6f787da1c5709b0ebe7c633be0d
Haider-Ali-7 <haideralitariqcheema@gmail.com>
2023-12-14 18:23:17 +0500

    notification-move-tap

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { LogBox } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  createNavigationContainerRef,
+} from "@react-navigation/native";
 import { enableFreeze, enableScreens } from "react-native-screens";
 import * as Sentry from "@sentry/react-native";
 import { withIAPContext } from "react-native-iap";
+import { useSelector } from "react-redux";
 
 import SocketProvider from "./src/context/SocketContext";
 import StackNavigations from "./src/Navigations/StackNavigation";
@@ -33,72 +37,49 @@ const { store } = configureStore();
 CallService.getStore(store);
 pushNotificationService.getStore(store);
 
-OneSignal.setAppId("04e507aa-792a-45f8-9d6d-55859c8dbd92");
-OneSignal.setLogLevel(6, 0);
-
-//Prompt for push on iOS
-OneSignal.promptForPushNotificationsWithUserResponse(res => {
-  console.log("Prompt response:", res);
-});
-
-//Method for handling notifications received while app in foreground
-OneSignal.setNotificationWillShowInForegroundHandler(
-  notificationReceivedEvent => {
-    let notification = notificationReceivedEvent.getNotification();
-
-    // Complete with null means don't show a notification.
-    notificationReceivedEvent.complete(notification);
-  }
-);
-
-//Method for handling notifications opened
-OneSignal.setNotificationOpenedHandler(notification => {
-  console.log("OneSignal: notification opened:", notification);
-});
-
 const checkToken = async () => {
   let login = await AsyncStorage.getItem("login");
   let cred = await AsyncStorage.getItem("cred");
 
   if (login != null && cred != null) {
     cred = await JSON.parse(cred);
-    createConnectyCubeSession(cred, login);
+    // createConnectyCubeSession(cred, login);
   }
 };
 
-const createConnectyCubeSession = (cred, login) => {
+const createConnectyCubeSession = () => {
   const CREDENTIALS = {
-    appId: cred?.appid,
-    authKey: cred?.authKey,
-    authSecret: cred?.authSec,
+    appId: 6703,
+    authKey: "7eQODLX7ZG2eSxZ",
+    authSecret: "RLAjfBYRt9gSFfs",
   };
   const CONFIG = {
-    debug: { mode: 0 },
+    debug: { mode: 1 },
   };
 
   ConnectyCube.init(CREDENTIALS, CONFIG);
 
-  const userCredentials = {
-    login: login,
-    password: "12345678",
-  };
+  // const userCredentials = {
+  //   login: login,
+  //   password: "12345678",
+  // };
 
-  ConnectyCube.createSession(userCredentials)
-    .then(session => {
-      ConnectyCube.chat
-        .connect({
-          userId: session.id,
-          password: session.token,
-        })
-        .then(() => {
-          CallService.init();
-          pushNotificationService.init();
-        })
-        .catch(error => {
-          console.log("error: ", error);
-        });
-    })
-    .catch(err => console.log("createSession err:", err));
+  // ConnectyCube.createSession(userCredentials)
+  //   .then(session => {
+  //     ConnectyCube.chat
+  //       .connect({
+  //         userId: session.id,
+  //         password: session.token,
+  //       })
+  //       .then(() => {
+  //         CallService.init();
+  //         pushNotificationService.init();
+  //       })
+  //       .catch(error => {
+  //         console.log("error: ", error);
+  //       });
+  //   })
+  //   .catch(err => console.log("createSession err:", err));
 };
 
 const requestUserPermission = async () => {
@@ -117,15 +98,63 @@ const notificationListener = async () => {
 };
 
 const App = () => {
+  const navigationRef = createNavigationContainerRef();
+  const { mobileNumber, email } = useSelector(store => store.userReducer);
+
+  const [isOpened, setIsOpened] = useState(false);
+
+  OneSignal.setAppId("04e507aa-792a-45f8-9d6d-55859c8dbd92");
+  OneSignal.setLogLevel(6, 0);
+
+  //Prompt for push on iOS
+  OneSignal.promptForPushNotificationsWithUserResponse(res => {
+    console.log("Prompt response:", res);
+  });
+
+  //Method for handling notifications received while app in foreground
+  OneSignal.setNotificationWillShowInForegroundHandler(
+    notificationReceivedEvent => {
+      let notification = notificationReceivedEvent.getNotification();
+      console.log("notification", JSON.stringify(notification, null, 2));
+      // Complete with null means don't show a notification.
+      notificationReceivedEvent.complete(notification);
+    }
+  );
+
+  //Method for handling notifications opened
+  OneSignal.setNotificationOpenedHandler(notification => {
+    console.log("OneSignal: notification opened:", notification);
+    if (mobileNumber || email) {
+      console.log("in if 0");
+      if (notification.notification.body.includes("received")) {
+        console.log("in if 1");
+        navigate("BottomTab");
+      }
+    }
+
+    setIsOpened(true);
+  });
+
+  const navigate = name => {
+    if (navigationRef.isReady()) {
+      setTimeout(() => {
+        navigationRef.navigate(name, {
+          screen: "Interactions",
+        });
+      }, 3300);
+    }
+  };
+
   useEffect(() => {
     requestUserPermission();
     notificationListener();
     // checkToken();
+    createConnectyCubeSession();
   }, []);
 
   return (
     <SocketProvider>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
         <StackNavigations />
         <Toast />
       </NavigationContainer>
