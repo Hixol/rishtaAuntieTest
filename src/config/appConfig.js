25e20fb9ddb20272c96092aa2e88e847f4829a15
TalhaHixol <talha.k@hixol.com>
2024-07-04 16:49:42 +0500

    Server url change

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,10 +1,13 @@
 export default appConfig = {
-  server_url: 'https://api.rishtaauntie.app/dev/rishta_auntie/api/v1',
+  // server_url: "https://api.rishtaauntie.app/dev/rishta_auntie/api/v1",
+
+  // New url
+  server_url: "https://api.rishtaauntie.link/dev/rishta_auntie/api/v1",
   server_url_old:
-    'https://rishtaauntie-app.herokuapp.com/dev/rishta_auntie/api/v1',
+    "https://rishtaauntie-app.herokuapp.com/dev/rishta_auntie/api/v1",
   server_url_aws:
-    'http://rishta-auntie-v2-dev.us-east-2.elasticbeanstalk.com/dev/rishta_auntie/api/v1',
-  audioPath: '/storage/emulated/0/Android/media/com.rishtaaunty/Audios/Sent',
-  localPath: '/storage/emulated/0/Android/media/com.rishtaaunty/',
-  filePath: '',
+    "http://rishta-auntie-v2-dev.us-east-2.elasticbeanstalk.com/dev/rishta_auntie/api/v1",
+  audioPath: "/storage/emulated/0/Android/media/com.rishtaaunty/Audios/Sent",
+  localPath: "/storage/emulated/0/Android/media/com.rishtaaunty/",
+  filePath: "",
 };
